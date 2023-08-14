8881aa22a8d59a89529f9dd37292a8df51a9e1be
NarcisCiovnicu <79647965+NarcisCiovnicu@users.noreply.github.com>
2023-08-14 15:06:37 +0300

    Queries improvements

--- DataAccess/Repositories/BooksRepository.cs
+++ DataAccess/Repositories/BooksRepository.cs
@@ -24,7 +24,8 @@ namespace DataAccess.Repositories
             connection.Open();
             using var transaction = connection.BeginTransaction();
 
-            var newId = await connection.QuerySingleAsync<int>("INSERT INTO Books(Title, Description, CoverPhoto) VALUES(@Title, @Description, @CoverPhoto) RETURNING Id", book);
+            var query = "INSERT INTO Books(Title, Description, CoverPhoto) VALUES(@Title, @Description, @CoverPhoto) RETURNING Id";
+            var newId = await connection.QuerySingleAsync<int>(query, book);
 
             await InsertAuthorIdsAsync(connection, transaction, newId, book.Authors);
 
@@ -33,31 +34,45 @@ namespace DataAccess.Repositories
 
         public async Task<IEnumerable<Book>> GetAllAsync()
         {
-            var queryBooks = "SELECT Id, Title, Description, CoverPhoto FROM Books";
-            var queryAuthors = @"SELECT Id, Name FROM Books_Authors BA
-                                JOIN Authors A ON A.Id = BA.AuthorId
-                                WHERE BA.BookId = @id";
+            var query = @"SELECT B.Id, B.Title, B.Description, B.CoverPhoto, A.Id, A.Name FROM Books B
+                        JOIN Books_Authors BA ON B.id = BA.BookID
+                        JOIN Authors A ON A.Id = BA.AuthorId;";
 
             using var connection = _dbContext.CreateConnection();
-            var books = await connection.QueryAsync<Book>(queryBooks);
-            foreach (var book in books)
-            {
-                book.Authors = (await connection.QueryAsync<Author>(queryAuthors, new { id = book.Id })).ToList();
-            }
 
-            return books;
+            var booksDictionary = new Dictionary<int, Book>();
+
+            var result = await connection.QueryAsync<Book, Author, Book>(query, (bookQuery, authorQuery) =>
+            {
+                if (booksDictionary.TryGetValue(bookQuery.Id, out Book? book))
+                {
+                    book.Authors.Add(authorQuery);
+                }
+                else
+                {
+                    bookQuery.Authors.Add(authorQuery);
+                    booksDictionary.Add(bookQuery.Id, bookQuery);
+                }
+
+                return bookQuery;
+            }, splitOn: "Id");
+
+            return booksDictionary.Values.ToList();
         }
 
         public async Task<Book> GetAsync(int id)
         {
-            var queryBook = "SELECT Id, Title, Description, CoverPhoto FROM Books WHERE Id = @id";
-            var queryAuthors = @"SELECT Id, Name FROM Books_Authors BA
+            var query = @"SELECT Id, Title, Description, CoverPhoto FROM Books WHERE Id = @id;
+                          SELECT Id, Name FROM Books_Authors BA
                                 JOIN Authors A ON A.Id = BA.AuthorId
-                                WHERE BA.BookId = @id";
+                                WHERE BA.BookId = @id;";
 
             using var connection = _dbContext.CreateConnection();
-            var book = await connection.QueryFirstAsync<Book>(queryBook, new { id });
-            book.Authors = (await connection.QueryAsync<Author>(queryAuthors, new { id })).ToList();
+
+            using var multi = await connection.QueryMultipleAsync(query, new { id });
+
+            var book = multi.Read<Book>().Single();
+            book.Authors = multi.Read<Author>().ToList();
 
             return book;
         }
@@ -68,8 +83,16 @@ namespace DataAccess.Repositories
             connection.Open();
             using var transaction = connection.BeginTransaction();
 
-            await connection.ExecuteAsync("UPDATE Books SET Title = @Title, Description = @Description, CoverPhoto = @CoverPhoto WHERE Id = @Id", book);
-            await connection.ExecuteAsync("DELETE FROM Books_Authors WHERE BookId = @id", new { id = book.Id });
+            var query = @"UPDATE Books SET Title = @Title, Description = @Description, CoverPhoto = @CoverPhoto WHERE Id = @BookId;
+                          DELETE FROM Books_Authors WHERE BookId = @BookId;";
+
+            await connection.ExecuteAsync(query, new
+            {
+                book.Title,
+                book.Description,
+                book.CoverPhoto,
+                BookId = book.Id
+            });
             await InsertAuthorIdsAsync(connection, transaction, book.Id, book.Authors);
 
             transaction.Commit();
@@ -81,8 +104,10 @@ namespace DataAccess.Repositories
             connection.Open();
             using var transaction = connection.BeginTransaction();
 
-            await connection.ExecuteAsync("DELETE FROM Books_Authors WHERE BookId = @id", new { id });
-            await connection.ExecuteAsync("DELETE FROM Books WHERE Id = @id", new { id });
+            var query = @"DELETE FROM Books_Authors WHERE BookId = @id;
+                          DELETE FROM Books WHERE Id = @id;";
+
+            await connection.ExecuteAsync(query, new { id });
 
             transaction.Commit();
         }
